1384ca9515df583fc7f03466a3329709140fb726
Phong <p7157568@gmail.com>
2016-03-30 15:23:52 +0700

    feat - search return success

--- app/src/main/java/dhbk/android/testgooglesearchreturn/MainActivity.java
+++ app/src/main/java/dhbk/android/testgooglesearchreturn/MainActivity.java
@@ -85,28 +85,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
@@ -114,13 +92,13 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart: bat dau");
+        Log.i(TAG, "onStart: bat dau");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop: bat dau");
+        Log.i(TAG, "onStop: bat dau");
     }
 
 }
